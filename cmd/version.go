3b0a723de5f5c9acaccd22a9afaf6313c0465c9d
Selyss <99344963+Selyss@users.noreply.github.com>
2023-11-28 19:10:29 -0500

    add: commands

--- cmd/version.go
+++ cmd/version.go
@@ -6,14 +6,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func init() {
-	rootCmd.AddCommand(versionCmd)
-}
-
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of GistSnip",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("GistSnip v0.9")
 	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
 }
